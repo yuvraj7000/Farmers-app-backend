9e8e7cb63740afaf33e0664917d19774bc99e241
Yuvraj Yadav <yuvraj7000raju@gmail.com>
2025-04-01 15:57:49 +0530

    feat: enhance todaysWeather function to handle errors and use query parameters

--- src/controllers/weather.controller.js
+++ src/controllers/weather.controller.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 
 
-const todaysWeather = async(req, res) => {
-    const { lat , lon} = req.body;
+const todaysWeather = async (req, res) => {
+    const { lat, lon } = req.query;
+    console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`);
     
-    const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
-    console.log("weather : ", weather)
+    try {
+        const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`);
+        console.log("weather : ", weather.data);
 
-
-    console.log("lat : ", lat)
-    console.log("lon : ", lon)
-    res.status(200).json({ message: 'Weather data for today', weather: weather.data });
-}
+        console.log("lat : ", lat);
+        console.log("lon : ", lon);
+        res.status(200).json({ message: 'Weather data for today', weather: weather.data });
+    } catch (error) {
+        console.error("Error fetching weather data: ", error.message);
+        res.status(500).json({ message: 'Failed to fetch weather data', error: error.message });
+    }
+};
 
 export { todaysWeather }
