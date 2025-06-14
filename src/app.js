56d1e035957adebf965b182f6f7bd17eba0cf78e
yuvraj yadav <yuvraj7000raju@gmail.com>
2025-06-14 19:04:03 +0530

    feat: update request body size limits for JSON and URL-encoded data

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ import newsRouter from './routers/news.router.js';
 
 const app = express();
 app.use(cors('*'));
-app.use(express.json({limit: '16kb'}));
-app.use(express.urlencoded({extended: true, limit: '16kb'}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true, limit: '100kb'}));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
